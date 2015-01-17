287bf73c95a5207acae4fb1a7ca9170eb9ab2067
dianlujitao <dianlujitao@gmail.com>
2015-01-17 21:42:13 +0800

    c8817d: cm bring up

--- cm.mk
+++ cm.mk
@@ -14,8 +14,14 @@
 # limitations under the License.
 #
 
-# Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/cdma.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := C8817D
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/huawei/c8817d/full_c8817d.mk)
@@ -24,5 +30,6 @@ $(call inherit-product, device/huawei/c8817d/full_c8817d.mk)
 PRODUCT_PACKAGES += \
     Torch
 
-PRODUCT_RELEASE_NAME := C8817D
-PRODUCT_NAME := mk_c8817d
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := c8817d
+PRODUCT_NAME := cm_c8817d
